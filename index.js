4d3c58b0891df0a73dc8fdb52b322efff1f6dbfe
damonmoya <damon99@hotmail.es>
2022-12-06 15:21:31 +0000

    Command deploy

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-//process.env 
-const dotenv = require('dotenv');
-
+const dotenv = require("dotenv");
 dotenv.config();
 
-const { Client, GatewayIntentBits, EmbedBuilder } = require("discord.js");
+const fs = require("node:fs");
+const path = require("node:path");
+
+const {
+  Client,
+  Events,
+  GatewayIntentBits,
+  Collection,
+} = require("discord.js");
 const client = new Client({
   intents: [
     GatewayIntentBits.DirectMessages,
@@ -14,39 +20,48 @@ const client = new Client({
   ],
 });
 
+client.commands = new Collection();
+
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs
+  .readdirSync(commandsPath)
+  .filter((file) => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+  if ("data" in command && "execute" in command) {
+    client.commands.set(command.data.name, command);
+  } else {
+    console.log(
+      `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+    );
+  }
+}
+
 client.on("ready", () => {
   console.log("¡Bot listo!");
 });
 
-client.on("messageCreate", (message) => {
-  if (message.content.startsWith("ping")) {
-    message.channel.send("pong!");
-  }
-});
+client.on(Events.InteractionCreate, async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
 
-// Asigna la función como manejador del comando "!embed"
-client.on("message", (message) => {
-  if (message.content === "!embed") {
-    handleEmbedCommand(
-      message,
-      "Título del mensaje",
-      "Descripción del mensaje"
-    );
+  const command = interaction.client.commands.get(interaction.commandName);
+
+  if (!command) {
+    console.error(`No command matching ${interaction.commandName} was found.`);
+    return;
+  }
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    await interaction.reply({
+      content: "There was an error while executing this command!",
+      ephemeral: true,
+    });
   }
 });
 
 client.login(process.env.DISCORD_TOKEN);
-
-// Crea una función que maneje el comando "!embed"
-function handleEmbedCommand(message, title, description) {
-  // Crea una instancia de RichEmbed
-  const embed = new EmbedBuilder()
-    .setTitle(title)
-    .setDescription(description)
-    .setColor(0x00ae86)
-    .setFooter("Pie de página del mensaje")
-    .setTimestamp();
-
-  // Envía el mensaje embebido al canal
-  message.channel.send(embed);
-}
